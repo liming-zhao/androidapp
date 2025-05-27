da77dd44539d9b331db3a1456b8d25418f266e12
Liming <leonzhaoemail@gmail.com>
2025-05-27 14:06:56 -0400

    adding a card image

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(files("C:\\Users\\linac\\Downloads\\exp4j-0.4.8.jar"))
+    implementation(files("libs\\exp4j-0.4.8.jar"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/fungame/MainActivity.kt
+++ app/src/main/java/com/example/fungame/MainActivity.kt
@@ -7,11 +7,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -25,9 +28,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.fungame.ui.theme.FunGameTheme
+
 import net.objecthunter.exp4j.ExpressionBuilder
 import kotlin.random.Random
 
@@ -173,15 +180,26 @@ fun Card(name: String, modifier: Modifier = Modifier) {
     val expanded = remember {mutableStateOf(false)}
     val extrapadding = if (expanded.value) 48.dp else 0.dp
     Surface(color = MaterialTheme.colorScheme.primary,
-        modifier = modifier.padding(vertical=4.dp,horizontal=8.dp)) {
-        Row(modifier = Modifier.padding((24.dp))) {
-            Column(modifier = modifier
-                .padding(bottom = extrapadding))
+        modifier = modifier.padding(vertical=14.dp,horizontal=8.dp)) {
+        Row(modifier = Modifier.padding((48.dp))) {
+            Column(modifier = modifier.padding(bottom = extrapadding))
 
             {
+                Image(
+                    painterResource(id=R.drawable.card2_of_clubs),
+                    contentDescription = null,
+
+                    modifier = Modifier.size(200.dp)
+                )
+
                 Text(
                     text = "Hello ",
                 )
+
+
+
+
+
                 Text(text = "       $name")
             }
 
